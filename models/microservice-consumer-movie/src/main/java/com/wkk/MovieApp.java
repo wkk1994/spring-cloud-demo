3b6382367fc5192888ed5aecaf97bdbe6ee90911
wangkunkun <wangkunkun@dhcc.com.cn>
2017-08-27 15:46:35 +0800

    服务者消费者注册到eureka

--- models/microservice-consumer-movie/src/main/java/com/wkk/MovieApp.java
+++ models/microservice-consumer-movie/src/main/java/com/wkk/MovieApp.java
@@ -2,6 +2,7 @@ package com.wkk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -9,6 +10,7 @@ import org.springframework.web.client.RestTemplate;
  * Created by Administrator on 2017/8/26 0026.
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class MovieApp {
     @Bean
     //会根据方法名作为bean name
--- models/microservice-provider-user/src/main/java/com/wkk/UserApp.java
+++ models/microservice-provider-user/src/main/java/com/wkk/UserApp.java
@@ -2,11 +2,13 @@ package com.wkk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Created by Administrator on 2017/8/26 0026.
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class UserApp {
     public static void main(String[] args){
         SpringApplication.run(UserApp.class,args);
